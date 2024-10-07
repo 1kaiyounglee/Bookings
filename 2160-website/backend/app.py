a1a1c2fe3d61d7218b1985e580084613aa258329
Kai Young Lee <kaiyounglee04@gmail.com>
2024-10-07 17:13:32 +1100

    added CORS to flask app

--- 2160-website/backend/app.py
+++ 2160-website/backend/app.py
@@ -2,27 +2,24 @@ from flask import Flask, jsonify
 import sys
 import os
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+from flask_cors import CORS
 from helper_modules import db_helper as db
 
 app = Flask(__name__)
-
+CORS(app)
 @app.route('/api/users', methods=['GET'])
 def get_users():
     try:
-        # Define the SQL query to fetch data from the Users table
         query = "SELECT * FROM Users"
-        
-        # Fetch the data using the fetch_data function from db_helper
         df = db.fetch_data(query)
 
-        # Convert the DataFrame to a list of dictionaries for JSON serialization
         if df is not None:
             users_data = df.to_dict(orient='records')
             return jsonify(users_data)
         else:
             return jsonify({'error': 'No data found'}), 404
-        print(f"An error occurred: {e}")
     except Exception as e:
+        print(f"An error occurred: {e}")
         return jsonify({'error': 'An error occurred while fetching the data.'}), 500
 
 if __name__ == "__main__":
