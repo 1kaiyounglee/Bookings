d4edbd704ff6b3de064f6a160cafcae04a1928bb
Kai Young Lee <kaiyounglee04@gmail.com>
2024-10-07 17:28:28 +1100

    created flask app and database api endpoint

--- 2160-website/backend/app.py
+++ 2160-website/backend/app.py
@@ -1,26 +1,19 @@
 from flask import Flask, jsonify
 import sys
 import os
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(os.path.dirname(os.path.abspath(__file__))) #fix pathing issues
+from flask import Flask
 from flask_cors import CORS
-from helper_modules import db_helper as db
+
+from api.api_db import api_db
 
 app = Flask(__name__)
-CORS(app)
-@app.route('/api/users', methods=['GET'])
-def get_users():
-    try:
-        query = "SELECT * FROM Users"
-        df = db.fetch_data(query)
 
-        if df is not None:
-            users_data = df.to_dict(orient='records')
-            return jsonify(users_data)
-        else:
-            return jsonify({'error': 'No data found'}), 404
-    except Exception as e:
-        print(f"An error occurred: {e}")
-        return jsonify({'error': 'An error occurred while fetching the data.'}), 500
+# Enable CORS for all routes
+CORS(app, resources={r"/api/*": {"origins": "*"}}, supports_credentials=True)
+
+# Register the database-related routes
+app.register_blueprint(api_db, url_prefix="/api/database")
 
 if __name__ == "__main__":
     app.run(debug=True)
