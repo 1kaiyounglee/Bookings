53b8056439add9ef6fd32701f493ea431685d5ca
Kai Young Lee <kaiyounglee04@gmail.com>
2024-10-10 22:43:24 +1100

    pkg

--- 2160-website/backend/test.py
+++ 2160-website/backend/test.py
@@ -1,6 +1,6 @@
 from helper_modules import db_helper as db, util as ut
 from datetime import datetime
-data = db.fetch_data(query="SELECT * FROM Users")
-print(data)
+# data = db.fetch_data(query="SELECT * FROM Users")
+# print(data)
 
-# ut.update_user("hjgkgjhka2dgfhf@fdgh", "testesttestetestest", True)
+ut.fetch_and_print_all_tables()
