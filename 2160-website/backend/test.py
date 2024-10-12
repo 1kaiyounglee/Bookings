b047d0b178a124a876dde88f97f4582e716cf239
Kai Young Lee <kaiyounglee04@gmail.com>
2024-10-12 22:17:43 +1100

    uupdated package page for balch labcl blah

--- 2160-website/backend/test.py
+++ 2160-website/backend/test.py
@@ -1,2 +1,6 @@
 from helper_modules import db_helper as db, util as ut
 from datetime import datetime
+print(db.fetch_data("SELECT * FROM Packages"))
+print(db.fetch_data("SELECT * FROM Categories"))
+print(db.fetch_data("SELECT * FROM PackageCategory"))
+
