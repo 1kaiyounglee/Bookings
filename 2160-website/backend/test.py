c10014da3fbc5d665f61af04483df4d55b512be5
Kai Young Lee <kaiyounglee04@gmail.com>
2024-10-13 16:30:11 +1100

    changed layout of package details page

--- 2160-website/backend/test.py
+++ 2160-website/backend/test.py
@@ -2,6 +2,6 @@ from helper_modules import db_helper as db, util as ut
 from datetime import datetime
 # print(db.fetch_data("SELECT * FROM Packages"))
 # print(db.fetch_data("SELECT * FROM Categories"))
-print(db.fetch_data("SELECT * FROM Bookings"))
-
+# print(db.fetch_data("SELECT * FROM Bookings"))
+ut.fetch_and_print_all_tables()
 
